fd1363e40fc0f8584e74446edb00185da6d164ad
Iphiclus <georginahall@yahoo.com>
2018-07-23 21:51:17 +0100

    Inital commit

---
+++ Exercise1-4/Exercise1-4/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+for far = 0, 20 ..., 300 */
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;		/* lower limt of temperatur table */
+	upper = 300;	/* upper limit */
+	step = 20;		/* step size */
+
+	printf("Converstion table of Celsius and Fahrenheit\n");
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+
+		printf("%6.1f %3.0f\n", celsius, fahr);
+		fahr = fahr + step;
+	}
+}
