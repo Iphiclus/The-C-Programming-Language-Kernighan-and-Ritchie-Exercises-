3931d3110c20e68fd7e6bae92a5ce860787ddfde
Iphiclus <georginahall@yahoo.com>
2018-07-24 23:36:38 +0100

    Initail commit, not finished. Will be used to practice GitHub branching.

---
+++ Exercise1-6/Exercise1-6/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/* copy input to output; 2nd version */
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+main()
+{
+	int c;
+
+	while ((c = getchar()) != EOF)
+
+		putchar(c);
+
+		//printf("%d\n", getchar() = EOF);
+	
+
+}
+
+
+/*
+main()
+{
+
+	main() {
+
+		while (1) printf("getchar() != EOF: %d\n", getchar() != EOF);
+
+	}
+
+
+
+}
+int main(void)
+
+{
+
+printf("Enter a char\n");
+
+printf("getchar() != EOF : %d\n", getchar() != EOF);
+
+return EXIT_SUCCESS;
+
+}
+*/
