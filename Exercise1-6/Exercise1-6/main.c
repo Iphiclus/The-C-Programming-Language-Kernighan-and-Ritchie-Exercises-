ee3a666e6be1dbac338ec162d8f4d267ee6b86af
Iphiclus <georginahall@yahoo.com>
2018-07-26 15:37:08 +0100

    Adding changes to Exercise 1-6.

--- Exercise1-6/Exercise1-6/main.c
+++ Exercise1-6/Exercise1-6/main.c
@@ -1,47 +1,13 @@
 #include <stdio.h>
 
-/* copy input to output; 2nd version */
+/* Exercise 1-6 */
 
-#include <stdio.h>
-
-#include <stdlib.h>
-
-main()
-{
-	int c;
-
-	while ((c = getchar()) != EOF)
-
-		putchar(c);
-
-		//printf("%d\n", getchar() = EOF);
-	
-
-}
-
-
-/*
-main()
-{
-
-	main() {
-
-		while (1) printf("getchar() != EOF: %d\n", getchar() != EOF);
-
-	}
-
-
-
-}
 int main(void)
 
 {
+	printf("Please type a word and press enter:\n");
 
-printf("Enter a char\n");
-
-printf("getchar() != EOF : %d\n", getchar() != EOF);
-
-return EXIT_SUCCESS;
+	printf("The expression \"getchar() != EOF\" is %d.\n", getchar() != EOF);
 
 }
-*/
+
