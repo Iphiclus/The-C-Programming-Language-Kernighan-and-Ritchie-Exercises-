79f4c39584c9abe7bbb961314bfa44d7203ccf9a
Georgina Hall <>
2019-05-08 13:31:15 +0100

    Tidied up exercise

--- Exercise1-3/main.c
+++ Exercise1-3/main.c
@@ -1,3 +1,16 @@
+/*
+ * Filename:    main.c
+ * Author:      Georgina Hall
+ * Date:        08-MAY-2019
+ *
+ * The C Programming Language, second edition,
+ * by Brian W. Kernighan and Dennis M. Ritchie
+ *
+ * Exercise 1-3, page 13
+ *
+ * Modify the temperature conversion program to print a heading above the table.
+ */
+
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
