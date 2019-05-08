36f615b89c4717ea7aabd59681f5bf05a33d5e30
Georgina Hall <>
2019-05-08 12:46:19 +0100

    init

---
+++ ExamplesChapter1-2/fahrenheit-celsiusTable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+    for fahr = 0, 20 ..., 300 */
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
---
+++ Exercise1-3/fahrenheit-to-celsius-convertion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+    for fahr = 0, 20 ..., 300 */
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    // Added in a heading
+    printf("Fahrenheit to Celsius conversion table:\n");
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
