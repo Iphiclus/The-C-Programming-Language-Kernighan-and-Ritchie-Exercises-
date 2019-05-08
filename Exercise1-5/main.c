7514d8e7631c1a5c4e048d58415ff8254e93e6a6
Georgina Hall <>
2019-05-08 14:00:52 +0100

    Update exercise

--- Exercise1-5/main.c
+++ Exercise1-5/main.c
@@ -1,11 +1,22 @@
+/*
+ * Filename:    main.c
+ * Author:      Georgina Hall
+ * Date:        08-MAY-2019
+ *
+ * The C Programming Language, second edition,
+ * by Brian W. Kernighan and Dennis M. Ritchie
+ *
+ * Exercise 1-5, The For Statement page 14
+ *
+ * Modify the temperature conversion program to print the table in reverse order from 300 degrees to 0.
+ */
+
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table*/
 main()
 {
 	int fahr;
 
 	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
 		printf("%3d %6.1f\n", fahr, (5.0 / 9.0)*(fahr-32));
-	
 }
