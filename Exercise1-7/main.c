c1e50d27fbb5702743eb3bb7798ba8b8498b4b4f
Georgina Hall <>
2019-05-08 16:12:29 +0100

    init

---
+++ Exercise1-7/main.c
@@ -0,0 +1,19 @@
+/*
+ * Filename:    main.c
+ * Author:      Georgina Hall
+ * Date:        08-MAY-2019
+ *
+ * The C Programming Language, second edition,
+ * by Brian W. Kernighan and Dennis M. Ritchie
+ *
+ * Exercise 1-7, page 17
+ *
+ * Modify the temperature conversion program to print a heading above the table.
+ */
+
+#include <stdio.h>
+
+main()
+{
+    printf("EOF: %d\n", EOF);
+}
